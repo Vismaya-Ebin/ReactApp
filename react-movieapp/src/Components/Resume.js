e7693b446cdd323fd53d9b73baa3bbd70a0facaa
Vismaya Wilson <vismayawilson3@gmail.com>
2022-01-09 16:35:01 +0530

    Resume

---
+++ react-movieapp/src/Components/Resume.js
@@ -0,0 +1,8 @@
+export function Resume(){
+    const styles ={ display:"flex",flexDirection:"row",backgroundColor:"red",justifyContent:"space-between",}
+    return(<div style={styles}>
+        <div>123</div>
+        <div>456</div>
+    </div>)
+
+}
